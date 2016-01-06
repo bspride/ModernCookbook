c77749a3597feeaddd4abbe5ef2a4eddf3617ff0
Michael Clarke <mclarke@trusimulation.com>
2016-01-06 00:19:39 -0500

    Add router service and auth service

---
+++ app/services/AuthService.js
@@ -0,0 +1,30 @@
+import when from 'when';
+import LoginActions from '../actions/LoginActions';
+
+class AuthService {
+	login(username, password) {
+		return this.handleAuth(when(
+			//call Parse
+		));
+	}
+	
+	logout() {
+		LoginActions.logoutUser();
+	}
+	
+	signup(username, password, extra) {
+		return this.handleAuth(when(
+			//call Parse
+		));
+	}
+	
+	handleAuth(loginPromise) {
+		return loginPromise
+			.then(function(response) {
+				LoginActions.loginUser();
+				return true;
+			});
+	}
+}
+
+export default new AuthService()
