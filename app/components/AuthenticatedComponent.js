e7f3dc5bf1b527ca947eb225e86dc105c0c7d2cc
Michael Clarke <mclarke@trusimulation.com>
2016-02-01 01:35:49 -0500

    Fix authenticatedComponent

--- app/components/AuthenticatedComponent.js
+++ app/components/AuthenticatedComponent.js
@@ -1,43 +1,43 @@
-import { Component } from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
+import { routeActions } from 'react-router-redux'
 
-function Auth (ComposedComponent) {
-	return class AuthenticatedComponent extends Component {
-		static willTransistionTo(transition) {
-			if (!LoginStore.isLoggedIn()) {
-				transition.redirect('/login', {}, {'nextPath': transition.path});
+export function requireAuth (ComposedComponent) {
+	class AuthenticatedComponent extends React.Component {
+		componentWillMount() {
+			this.checkAuth()
+		}
+		
+		componentWillReceiveProps() {
+			this.checkAuth()
+		}
+		
+		checkAuth() {
+			if (!this.props.isAuthenticated) {
+				let path = '/login?next=' + this.props.pathname + this.props.search
+				this.props.dispatch(routeActions.push(path))
 			}
-		}
-		
-		constructor() {
-			super()
-			this.state = this._getLoginState();
-		}
-		
-		_getLoginState() {
-			return {
-				userLoggedIn: LoginStore.isLoggedIn()
-			};
-		}
-		
-		componentDidMount() {
-			this.changeListener = this._onChange.bind(this);
-			LoginStore.addChangeListener(this.changeListener);
-		}
-		
-		_onChange() {
-			this.setState(this._getLoginState());
-		}
-		
-		componentWillUnmount() {
-			LoginStore.removeChangeListener(this.changeListener);
 		}
 		
 		render() {
 			return (
-				<ComposedComponent
-					{...this.props}
-					userLoggedIn={this.state.userLoggedIn} />
-			);
+				<div>
+					{this.props.isAuthenticated === true
+						? <Component {...this.props}/>
+						: null
+					}
+				</div>
+			)
 		}
 	}
+	
+	function mapStateToProps (state){
+		return {
+			isAuthenticated: state.auth.isLoggedIn,
+			pathname: state.routing.location.pathname,
+			search: state.routing.location.search
+		}
+	}
+	
+	return connect(mapStateToProps)(AuthenticatedComponent)
 }
--- app/routes.js
+++ app/routes.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Route , IndexRoute} from 'react-router'
+import {requireAuth} from './components/AuthenticatedComponent'
 
-import Login from './components/Login';
-import CookbookApp from './containers/Cookbook/CookbookApp';
-import Signup from './components/Signup';
-import Home from './components/Home';
-import Index from './components/Index';
-import Create from './containers/Cookbook/Recipes';
+import Login from './components/Login'
+import CookbookApp from './containers/Cookbook/CookbookApp'
+import Signup from './components/Signup'
+import Home from './components/Home'
+import Index from './components/Index'
+import Create from './containers/Cookbook/Recipes'
 
 export default (
   <Route path="/" component={CookbookApp}>
 		<IndexRoute component={Index} />
 		<Route path="login" component={Login} />
 		<Route path="signup" component={Signup} />
-		<Route path="home" component={Home} />
-				 <Route path="create" component={Create} />
+		<Route path="home" component={requireAuth(Home)} />
+				 <Route path="create" component={requireAuth(Create)} />
 	</Route>
 )
