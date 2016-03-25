8440e2bf8e30aa8d1485dc93c4abb9033f617ce1
Robert Castillo <rcastillo2589@gmail.com>
2016-03-25 17:31:14 -0500

    Created another component and made some CSS changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import {FORM_PROVIDERS} from 'angular2/common';
 
 import '../style/app.scss';
 
+import {User} from './interfaces/user';
 import {Api} from './services/api/api';
 import {Home} from './components/home/home';
 import {About} from "./components/about/about";
-import {LoginSmall} from './components/login/login-small';
+import {Registration} from './components/login/registration';
 
 /*
  * App Component
@@ -16,18 +17,31 @@ import {LoginSmall} from './components/login/login-small';
 @Component({
   selector: 'app', // <app></app>
   providers: [...FORM_PROVIDERS, Api],
-  directives: [...ROUTER_DIRECTIVES, LoginSmall],
+  directives: [...ROUTER_DIRECTIVES],
   pipes: [],
   styles: [require('./app.scss')],
   template: require('./app.html')
 })
 @RouteConfig([
   {path: '/', component: Home, name: 'Home'},
-  {path: '/About', component: About, name: 'About'}
+  {path: '/About', component: About, name: 'About'},
+  {path: '/Register', component: Registration, name: 'Registration'}
 ])
 export class App {
+  isLoggedIn: boolean;
+  user: User;
   url: string = 'https://github.com/preboot/angular2-webpack';
 
   constructor(public api: Api) {
+    this.isLoggedIn = false;
+    this.user = {
+      id: "0",
+      name: "test",
+      email: "test@test.com"
+    };
+  }
+  
+  onRegister() {
+      window.location.hash = "#/Register";
   }
 }
--- src/app/components/login/login-small.ts
+++ src/app/components/login/login-small.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from 'angular2/core';
 
-import {User} from '../../interfaces/user';
-
 @Component({
   selector: 'login-small',
   template: require('./login-small.html'),
@@ -11,17 +9,10 @@ import {User} from '../../interfaces/user';
   pipes: []
 })
 export class LoginSmall implements OnInit {
-  isLoggedIn: boolean;
-  user: User;
+  
   
   constructor() {
-    // Make a call to check if the user is still logged in
-    this.isLoggedIn = false;
-    this.user = {
-      id: "0",
-      name: "test",
-      email: "test@test.com"
-    }
+
   }
 
   ngOnInit() {
