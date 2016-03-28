26d759b368217c2b3ce4ace6a66c33e6aad91f6a
Robert Castillo <rcastillo2589@gmail.com>
2016-03-27 20:17:36 -0500

    Added some authentication with firebase

--- src/app/services/auth-api/authentication.ts
+++ src/app/services/auth-api/authentication.ts
@@ -1,6 +1,28 @@
 import {Injectable} from 'angular2/core';
+import Firebase = require('firebase');
 
 @Injectable()
-export class Api {
-  title: string = 'Modern Cookbook';
+export class AuthApi {
+    firebaseUrl: string;
+    ref: Firebase;
+    
+    constructor() {
+        this.firebaseUrl = "https://sizzling-fire-4278.firebaseio.com/";
+        this.ref = new Firebase(this.firebaseUrl);
+    }
+    
+    registerUser(email: string, password: string) {
+        // Return promise to the initial caller
+        // return this.ref.createUser({
+        //     email: email,
+        //     password: password
+        // });
+    }
+    
+    authenticate(email: string, password: string) {
+        return this.ref.authWithPassword({
+            email: email,
+            password: password
+        });
+    }
 }
