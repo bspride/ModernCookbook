21d1c5d4316ee77134911840a346b3a25209a22c
Robert Castillo <rcastillo2589@gmail.com>
2016-04-19 21:51:39 -0500

    Removed sub-components and created one form to add recipes

--- src/app/components/createRecipe/createRecipe.ts
+++ src/app/components/createRecipe/createRecipe.ts
@@ -2,9 +2,6 @@ import {Component, OnInit} from 'angular2/core';
 import {RouteConfig, Router} from 'angular2/router';
 
 import {Security} from '../security/security';
-import {Title} from './title/title';
-import {Ingredients} from './ingredients/ingredients';
-import {Steps} from './steps/steps';
 
 import {Api} from '../../services/api/api';
 import {Create} from '../../services/create/create';
@@ -18,109 +15,35 @@ const STEPS = "Steps";
     template: require('./createRecipe.html'),
     styles: [require('./createRecipe.scss')],
     providers: [Create],
-    directives: [Title, Ingredients, Steps],
+    directives: [],
     pipes: []
 })
-export class CreateRecipe {
-    // These variables control the steps for creating
-    // a recipe
-    processId: number = 0;
-    allProcesses: Array<string> = [TITLE, INGREDIENTS, STEPS];
-    currentProcess: string;
-    
+export class CreateRecipe {    
     // Recipe specific variables
-    isPublic: boolean;
+    isPrivate: boolean;
     title: string;
-    ingredients: Array<string>;
-    steps: Array<string>;
+    ingredientsString: string;
+    stepsString: string;
+    
+    private ingredients: Array<string>;
+    private steps: Array<string>;
     
     constructor(
         private _router: Router, 
         private _api: Api,
         private _create: Create
-    ) {
-        this.currentProcess = this.allProcesses[0];
-        this.ingredients = [];
-        this.steps = [];
-    }
+    ) { this.isPrivate = false; }
     
-    addedTitle(newTitle) {
-        this.title = newTitle;
-        console.log(this.title);
-        // Navigate to next process after adding title
-        this.goToNextProcess();
-    }
-    
-    addedIngredient(ingredient) {
-        // Parse ingredient for name and amount
-        if(ingredient !== null) {
-            if(this.ingredients[0] === '') {
-                this.ingredients[0] = ingredient;
-            } else {
-                this.ingredients.push(ingredient);    
-            }
-        }
-        // this.goToNextProcess();
-    }
-    
-    addedStep(step) {
-        // Do additional processing for steps
-        if(step !== null) {
-            if(this.steps[0] === '') {
-                this.steps[0] = step;
-            } else {
-                this.steps.push(step);
-            }
-        }
-        // this.goToNextProcess();
-    }
-    
-    saveProcess() {
-        if(this.processId < this.allProcesses.length) {
-            // Check the current process and save if necessary
-            switch(this.currentProcess) {
-                case TITLE:
-                    this._create.saveTitle();
-                    break;
-                case INGREDIENTS:
-                    this._create.saveIngredients();
-                    break;
-                case STEPS:
-                    this._create.saveSteps();
-                    break;
-                default:
-                    // TODO Something weird happened
-                    break;
-            }
-        }
-    }
-    
-    goToNextProcess() {
-        if(this.processId < this.allProcesses.length) {
-            this.processId++;
-            this.currentProcess = this.allProcesses[this.processId];
-        }
-    }
-    
-    goToPreviousProcess() {
-        if(this.processId > 0) {
-            this.processId--;
-            this.currentProcess = this.allProcesses[this.processId];
-        }
-    }
-    
-    triggerNextProcess() {
-        this.goToNextProcess();
-    }
-    
-    saveRecipe(isPublic) {
+    saveRecipe() {
         let link = ['MyRecipe'];
-        this.isPublic = isPublic;
         
+        this.ingredients = this.parseIngredients();
+        this.steps = this.parseSteps();
+
         let recipe = {
             ingredients: this.ingredients,
             steps: this.steps,
-            isPublic: this.isPublic,
+            isPrivate: this.isPrivate,
             title: this.title,
         };
         console.log(recipe);
@@ -132,5 +55,17 @@ export class CreateRecipe {
             .catch(err => {
                 console.log("Failed")    
             });
+    }
+    
+    parseIngredients() {
+        if(this.ingredientsString !== null && this.ingredientsString.length > 0) {
+            return this.ingredientsString.split(/\r?\n/);
+        }
+    }
+    
+    parseSteps() {
+        if(this.stepsString !== null && this.stepsString.length > 0) {
+            return this.stepsString.split(/\r?\n/);
+        }
     }
 }
--- src/app/services/create/create.ts
+++ src/app/services/create/create.ts
@@ -3,24 +3,54 @@ import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class Create {
-    private _title = new Subject<boolean>();
-    private _ingredients = new Subject<boolean>();
-    private _steps = new Subject<boolean>();
+    private _isComplete: boolean;
+    
+    private _title: string;
+    private _ingredients: Array<string>;
+    private _steps: Array<string>;
+    
+    private _titleSubject = new Subject<string>();
+    private _ingredientsSubject = new Subject<Array<string>>();
+    private _stepsSubject = new Subject<Array<string>>();
     
     // Observable login check stream (boolean)
-    isTitleSaved$ = this._title.asObservable();
-    isIngredientsSaved$ = this._ingredients.asObservable();
-    isStepsSaved$ = this._steps.asObservable();
+    isTitleSaved$ = this._titleSubject.asObservable();
+    isIngredientsSaved$ = this._ingredientsSubject.asObservable();
+    isStepsSaved$ = this._stepsSubject.asObservable();
     
-    constructor() {}
-    
-    saveTitle() {
-        this._title.next(true);
+    constructor() {
+        this._isComplete = false;
     }
-    saveIngredients() {
-        this._ingredients.next(true);
+    /**
+     * Add new title and update all subcribers
+     */
+    saveTitle(newTitle) {
+        this._title = newTitle;
+        this._titleSubject.next(newTitle);
     }
-    saveSteps() {
-        this._steps.next(true);
+    /**
+     * Add new ingedients to array and update all subcribers
+     */
+    saveIngredients(newIngredient) {
+        this._ingredients.push(newIngredient)
+        this._ingredientsSubject.next(this._ingredients);
+    }
+    /**
+     * Add new steps to array and update all subcribers
+     */
+    saveSteps(newStep) {
+        this._steps.push(newStep);
+        this._stepsSubject.next(this._steps);
+    }
+    /**
+     * Persist the changes to the database and set 
+     * flag if this needs to be updated
+     */ 
+    saveRecipe(isComplete) {
+        if(isComplete) {
+            this._title = null;
+            this._ingredients = [];
+            this._steps = [];
+        }
     }
 }
